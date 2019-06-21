579c7d81b1a6b3c2c8c673e447fabe25c5bf0fc3
leaderg <leader.g@gmail.com>
2019-06-21 01:46:00 +0000

    Fixed some issues.

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './App';
-import Study from './study';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
