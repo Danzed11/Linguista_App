4c5cc5a042a3aeeb34d62d6cd803bb0ab374fff5
KateDay <khummle@gmail.com>
2019-06-22 12:18:07 -0600

    logo and overlay

--- react-front-end/src/components/nav.js
+++ react-front-end/src/components/nav.js
@@ -7,7 +7,11 @@ class Nav extends Component {
 			<nav>
 				<div className="nav-left">
 					{/* <h1>Linguista</h1> */}
-					<img src="../public/images/logo1.png" alt="Linguista" />
+					<img
+						className="logo"
+						src="https://github.com/Danzed11/Linguista_App/blob/master/react-front-end/public/logo/logoLG-Col.png?raw=true"
+						alt="Linguista"
+					/>
 				</div>
 				<div className="nav-right">
 					<h4>Hi User</h4>
