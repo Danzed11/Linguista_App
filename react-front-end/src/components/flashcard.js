1130f5a9c448837fc3803359bbc341aefd7daea1
KateDay <khummle@gmail.com>
2019-06-25 15:59:33 -0600

    dash - minor page position fix

--- react-front-end/src/components/flashcard.js
+++ react-front-end/src/components/flashcard.js
@@ -5,9 +5,7 @@ class Flashcard extends Component {
 	render() {
 		return (
 			<div className="flashcardContainer">
-				<h4>this is the flashcard box</h4>
-				<h1>{this.props.message}</h1>
-				<button onClick={this.fetchData}>Fetch Data</button>
+				<h4>Click here to study your words</h4>
 			</div>
 		);
 	}
