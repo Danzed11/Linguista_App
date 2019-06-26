3f6d5fb3bb303497fa3981de0dee78b283839581
KateDay <khummle@gmail.com>
2019-06-25 22:54:54 -0600

    Dashboard: set up link button for flashcard game

--- react-front-end/src/components/flashcard.js
+++ react-front-end/src/components/flashcard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../stylesheets/flashcard.css';
+import { Link } from 'react-router-dom';
 
 class Flashcard extends Component {
 	render() {
 		return (
-			<div className="flashcardContainer">
-				<h4>Click here to study your words</h4>
-			</div>
+			<button className="button flashcardContainer">
+				<Link to="/study" style={{ textDecoration: 'none', color: '#13292a' }}>
+					<h1>Click here to study your words</h1>
+				</Link>
+			</button>
 		);
 	}
 }
