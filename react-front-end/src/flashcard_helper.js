f23de458253382c0f3374200b1ffd5ba32c81502
leaderg <leader.g@gmail.com>
2019-06-30 20:23:51 +0000

    Scrapping recusion pattern in flashcard helper.

--- react-front-end/src/flashcard_helper.js
+++ react-front-end/src/flashcard_helper.js
@@ -2,52 +2,7 @@ class Flashcards {
   constructor(wordList) {
     this.cardlist = wordList;
     this.roundNumber = 0;
-    this.order = this.cardOrder(this.highestInterval(this.cardlist));
-  }
-
-  flatten(items) {
-    const flat = [];
-    let self = this;
-
-    items.forEach(item => {
-      if (Array.isArray(item)) {
-        flat.push(...self.flatten(item));
-      } else {
-        flat.push(item);
-      }
-    });
-    return flat;
-  }
-
-  recursiveOrder (n) {
-    let self = this
-    if (n !== 0) {
-      let output = []
-      output.push(self.recursiveOrder(n-1))
-      output.push(self.recursiveOrder(n-1))
-      output.push(n);
-      return output
-    }
-    return
-  }
-
-  cardOrder (highestInt) {
-    let output = this.recursiveOrder(highestInt)
-    output = this.flatten(output)
-    output = output.filter(function (el) {
-      return el != null;
-    });
-    return output
-  }
-
-  highestInterval (db) {
-    let highestNumb = 1;
-    for (let word of db) {
-      if (word.interval > highestNumb) {
-        highestNumb = word.interval
-      }
-    }
-    return highestNumb
+    this.order = [1,1,2,1,3,1,2,1]
   }
 
   card() {
