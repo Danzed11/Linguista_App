950bd7472b40fa42e3153952de9757ba3e207063
KateDay <khummle@gmail.com>
2019-07-03 09:08:57 -0600

    DropZone: input btns stacked

--- react-front-end/src/components/epubUploadForm.js
+++ react-front-end/src/components/epubUploadForm.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 
 class UploadForm extends Component {
-  render() {
-    return (
-    <form
-        id        =  "uploadForm"
-        encType   =  "multipart/form-data"
-        action    =  "/upload"
-        method    =  "post"
-    >
-    <lable>Book Title</lable><br/>
-    <input type="text" name="title" required /><br/>
-    <input type="file" name="uploadedEpub" required />
-    <input type="submit" value="Upload ePub" name="submit"/>
-    </form>
-    );
-  }
+	render() {
+		return (
+			<form
+				id="uploadForm"
+				encType="multipart/form-data"
+				action="/upload"
+				method="post"
+			>
+				<lable>Book Title</lable>
+				<br />
+				<input type="text" name="title" required />
+				<br />
+				<input type="file" name="uploadedEpub" required />
+				<br />
+				<input type="submit" value="Upload ePub" name="submit" />
+			</form>
+		);
+	}
 }
 
 export default UploadForm;
